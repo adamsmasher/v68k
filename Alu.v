dcdb8d48c6836a5aca9ce025fce5b60d6dc33e12
Adam Smith <unfactorable.polynomial@gmail.com>
2018-05-21 21:03:48 -0700

    let the ALU do more than add

--- Alu.v
+++ Alu.v
@@ -2,6 +2,7 @@ module Alu #(parameter bits=16) (
   input [bits-1:0] A,
   input [bits-1:0] B,
   output [bits-1:0] O,
+  input [2:0] op,
   input X,
   output C,
   output Z,
@@ -9,11 +10,25 @@ module Alu #(parameter bits=16) (
   output N
 );
 
+localparam ADD = 3'b000;
+localparam SUB = 3'b001;
+localparam AND = 3'b010;
+localparam OR  = 3'b011;
+localparam XOR = 3'b100;
+// TODO: further cases
+
 // one extra bit for carry
 reg [bits:0] result;
 
 always @(*) begin
-  result <= A + B + X;
+  case(op)
+    ADD: result <= A + B + X;
+    SUB: result <= A - B - X;
+    AND: result <= A & B;
+    OR: result <= A | B;
+    XOR: result <= A ^ B;
+    default: result <= A; // TODO: ???
+  endcase
 end
 
 assign O = result[bits-1:0];
--- Alu_test.v
+++ Alu_test.v
@@ -5,6 +5,7 @@ parameter bits = 16;
 reg [bits-1:0] a;
 reg [bits-1:0] b;
 wire [bits-1:0] o;
+reg [2:0] op = 0; // add
 reg x = 0;
 wire c;
 wire z;
@@ -13,7 +14,7 @@ wire n;
 
 assign TEST = 1;
 
-Alu #(bits) alu(a, b, o, x, c, z, v, n);
+Alu #(bits) alu(a, b, o, op, x, c, z, v, n);
 
 initial begin
   // zero
